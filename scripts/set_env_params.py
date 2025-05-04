83ee7edaf92c04209ca3a48fe2b9831f25e3b1ac
alexnavtt <alexnavtt@utexas.edu>
2025-05-04 11:56:39 -0600

    Added a script for setting the correct env variables for docker-compose

---
+++ scripts/set_env_params.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import yaml
+import subprocess
+
+def main(config_file: str):
+    with open(config_file, 'r') as file:
+        config_dict = yaml.safe_load(file)
+
+    output_dict = dict()
+
+    # ROS1 config
+    if ros1_config := config_dict.get('ros1', None):
+        output_dict['ROS_BRIDGE_GRPC_ROS1_DISTRO'] = ros1_config.get('distro', 'noetic')
+        output_dict['ROS_BRIDGE_GRPC_ROS1_URI'] = ros1_config.get('ROS_MASTER_URI', 'http://localhost:11311')
+        if ros_hostname := ros1_config.get('ROS_HOSTNAME', None):
+            output_dict['ROS_BRIDGE_GRPC_ROS1_HOSTNAME'] = ros_hostname
+        if ros_ip := ros1_config.get('ROS_IP', None):
+            output_dict['ROS_BRIDGE_GRPC_ROS1_IP'] = ros_ip
+
+    # ROS2 config
+    if ros2_config := config_dict.get('ros2', None):
+        output_dict['ROS_BRIDGE_GRPC_ROS2_DISTRO'] = ros2_config.get('distro', 'humble')
+        if ros_domain_id := ros2_config.get('ROS_DOMAIN_ID', None):
+            output_dict['ROS_BRIDGE_GRPC_ROS_DOMAIN_ID'] = ros_domain_id
+        if rmw_implementation := ros2_config.get('RMW_IMPLEMENTATION', 'cyclonedds_cpp'):
+            output_dict['ROS_BRIDGE_GRPC_RMW_IMPLEMENTATION'] = rmw_implementation
+        if cyclonedds_uri := ros2_config.get('CYCLONEDDS_URI', None):
+            output_dict['ROS_BRIDGE_GRPC_CYCLONEDDS_URI'] = cyclonedds_uri
+        
+    # Message types
+    system_packages = ""
+    allowed_types = ""
+    for msg_package, message_types in config_dict.get('msg_packages', dict()).items():
+        system_packages += f'{msg_package} '
+        for msg_type in message_types:
+            allowed_types += f'{msg_package}/{msg_type} '
+    output_dict['ROS_BRIDGE_GRPC_SYSTEM_PACKAGES'] = system_packages
+    output_dict['ROS_BRIDGE_GRPC_ALLOWED_TYPES'] = allowed_types
+
+    # Custom message types
+    if custom_packages := config_dict.get('custom_packages', None):
+        git_urls = ""
+        for repo_clone_url in custom_packages.get('git_urls', list[str]()):
+            git_urls += f'{repo_clone_url};'
+        output_dict['ROS_BRIDGE_GRPC_USER_REPOS'] = git_urls
+
+        source_dir = os.path.abspath(__file__)
+        mapped_dir = os.path.join(source_dir, '..', 'docker', 'mapped')
+
+        for local_package in custom_packages.get('package_paths', list[str]()):
+            _, target_dir_name = os.path.split(local_package)
+            print(f'{local_package=}, {mapped_dir=}, {target_dir_name=}')
+            subprocess.run(['mount', '--bind', local_package, os.path.join(mapped_dir, target_dir_name)])
+
+    # Compatibiltiy overrides
+    output_dict['ROS_BRIDGE_GRPC_COMPAT_OVERRIDES'] = ' '.join(config_dict.get('compatibility_overrides', list[str]()))
+
+    # Print the commands to be executed in the shell
+    for key, value in output_dict.items():
+        print(f'export {key}="{value}"')
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        raise RuntimeError('No configuration file provided!')
+
+    filename = sys.argv[1]
+    main(filename)
