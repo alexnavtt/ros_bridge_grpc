513ba07a9fed8001c5090b122a02c0488340d3c1
alexnavtt <alexnavtt@utexas.edu>
2025-05-04 20:17:42 -0600

    Cleaned up the cleanup routine of the buils script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,15 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 eval "$(python3 set_env_params.py $1)"
-echo "USER REPOS: " $ROS_BRIDGE_GRPC_USER_REPOS
 cd ../docker
 docker compose build
 
-for file in ../docker/mapped/*; do
-    [ "$(basename "$file")" = ".gitkeep" ] && continue  # Skip .gitkeep
-    sudo umount $file
+for file in ../docker/mapped/ros1/*; do
+    (umount "$file") || true;
+    rmdir $file
+done
+
+for file in ../docker/mapped/ros2/*; do
+    (umount $file) || true;
+    rmdir $file
 done
