a8a2f44d9c329be4c624bb36d63b4a25bb66b079
alexnavtt <alexnavtt@utexas.edu>
2025-05-04 20:17:04 -0600

    Updated to fix issues with passing ros params

--- docker/entrypoints/run_ros2_bridge.bash
+++ docker/entrypoints/run_ros2_bridge.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 source ~/.profile
-ros2 run ros2_bridge_grpc bridge_server_ros2 --ros-args --params-file /ros_params.yaml
+
+# Convert the ROS1 params file to a valid ROS2 params file
+echo "bridge_server_ros2:" >> /ros2_params.yaml
+echo "  ros__parameters:" >> /ros2_params.yaml
+sed 's/^/       /' /ros_params.yaml >> /ros2_params.yaml
+
+ros2 run ros2_bridge_grpc bridge_server_ros2 --ros-args --params-file /ros2_params.yaml
