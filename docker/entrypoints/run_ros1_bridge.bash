f5e9bd33d59e38d09732816d9fe99358a56f447a
alexnavtt <alexnavtt@utexas.edu>
2025-05-22 16:28:09 -0600

    Added a wait for external roscore

--- docker/entrypoints/run_ros1_bridge.bash
+++ docker/entrypoints/run_ros1_bridge.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 source ~/.profile
-sleep 2
+
+# Wait for roscore
+echo "Waiting for ROS master..."
+until rostopic list > /dev/null 2>&1; do
+  sleep 1
+done
+echo "Connected to ROS master!"
+
 rosparam load /ros_params.yaml 
 rosrun ros1_bridge_grpc bridge_server_ros1
