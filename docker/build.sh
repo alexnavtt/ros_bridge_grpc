159034ae860dfd69e549313f8e40b36fa2e3204c
alexnavtt <alexnavtt@utexas.edu>
2025-07-20 19:37:39 -0600

    Added profile to build script

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ mkdir -p mapped/ros1
 mkdir -p mapped/ros2
 
 eval "$(sudo -E python3 ../scripts/set_env_params.py $ROS_BRIDGE_BUILD_CONFIG)"
-docker compose build
+docker compose --profile all build
 
 for file in ../docker/mapped/ros1/*; do
     (sudo umount "$file") || true;
